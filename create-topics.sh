b9e0ddb3dec1d928752c361d91d38dc7b7bdbde8
plearning2001 <p.learning2001@gmail.com>
2025-12-13 20:39:10 +0530

    Producer connected with kafka container

--- create-topics.sh
+++ create-topics.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "Creating Kafka topics..."
+echo "Waiting for Kafka to be ready..."
+sleep 10
 
 kafka-topics \
-  --bootstrap-server kafka:9092 \
+  --bootstrap-server kafka:29092 \
   --create \
   --if-not-exists \
   --topic transactions \
   --partitions 1 \
   --replication-factor 1
 
-echo "Topics created"
+echo "Topic created successfully"
